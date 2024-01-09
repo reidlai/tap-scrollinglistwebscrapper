d026b6a879ed3b66172a504b5ad5842f9327b174
Reid Lai <reidlai@codelity.co>
2024-01-09 10:30:29 -0500

    update client

--- tap_scrollinglistwebscrapper/client.py
+++ tap_scrollinglistwebscrapper/client.py
@@ -9,6 +9,7 @@ from singer_sdk.streams import Stream
 
 import time
 from playwright.async_api import async_playwright
+import asyncio
 
 class ScrollingListWebScrapperStream(Stream):
 
@@ -31,29 +32,35 @@ class ScrollingListWebScrapperStream(Stream):
     @property
     def itemSelector(self) -> str:
         return self.config["itemSelector"]
-    
-    def get_records(self) -> Iterable[dict]:
-        pw = await async_playwright().start()
-        browser = await pw.chromium.launch(headless=True, timeout=browserTimeoutSeconds * 1000)
-        page = await browser.new_page()
-        await page.goto(url)
-        await scrollPageDown(page=page, scrollWaitSeconds=scrollWaitSeconds, endOfListSelector=endOfListSelector)
-        itemList = await getItemList(page=page, itemSelector=itemSelector)
-        await browser.close()
-        await pw.end()
+        
+    def get_records(self, context: dict | None) -> Iterable[dict]:
+        pw = asyncio.run(async_playwright().start())
+        browser = asyncio.run(pw.chromium.launch(headless=True, timeout=self.browserTimeoutSeconds * 1000))
+        page = asyncio.run(browser.new_page())
+        asyncio.run(page.goto(self.url))
+  
+        asyncio.run(self.scrollPageDown(page=page))
+        itemList = asyncio.run(getItemList(page=page))
+        asyncio.run(browser.close())
+        asyncio.run(pw.end())
         for item in itemList:
             yield dict(item=item)
         
-    async def scrollPageDown(page, scrollWaitSeconds, endOfPageSelector):
+                
+    async def scrollPageDown(self, page):
         selector = None
         while(selector == None):
             await page.keyboard.press("PageDown")
-            time.sleep(scrollWaitSeconds)
-            selector = await page.query_selector(endOfPageSelector)
+            time.sleep(self.scrollWaitSeconds)
+            selector = await page.query_selector(self.endOfPageSelector)
     
-    async def getItemList(page, itemSelector):
-        itemSelectorList = await page.query_selector_all(itemSelector)
+    async def getItemList(self, page):
+        itemSelectorList = await page.query_selector_all(self.itemSelector)
         itemList = []
         for itemSelector in itemSelectorList:
             itemList.append(await itemSelector.inner_html())
         return itemList
+    
+    schema = th.PropertiesList(
+        th.Property("item", th.StringType),
+    ).to_dict()
