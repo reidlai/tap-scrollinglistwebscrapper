45eb21568553d25e2838d650f5d807ddbed0a8fc
Reid Lai <reidlai@codelity.co>
2024-01-09 15:25:35 -0500

    initialize test_core

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,23 +1,20 @@
-"""Tests standard tap features using the built-in SDK tests library."""
-
-import datetime
 
 from singer_sdk.testing import get_tap_test_class
 
-from tap_scrollinglistwebscrapper.tap import Tapscrollinglistwebscrapper
+from tap_scrollinglistwebscrapper.tap import TapScrollingListWebScrapper
 
 SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
-    # TODO: Initialize minimal tap config
+    "url": "https://www.tntsupermarket.com/eng/weekly-special-er.html",
+    "browserTimeoutSeconds": 0,
+    "scrollWaitSeconds": 1,
+    "endOfListSelector": "p[class^='category-noData-']",
+    "itemSelector": "div[class^='item-root']"
 }
 
 
 # Run standard built-in tap tests from the SDK:
-TestTapscrollinglistwebscrapper = get_tap_test_class(
-    tap_class=Tapscrollinglistwebscrapper,
+TestTapScrollingListWebScrapper = get_tap_test_class(
+    tap_class=TapScrollingListWebScrapper,
     config=SAMPLE_CONFIG,
 )
-
-
-# TODO: Create additional tests as appropriate for your tap.
 
